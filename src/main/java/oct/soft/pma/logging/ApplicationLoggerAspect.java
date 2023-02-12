6765898a5413232cc404cf3b25493f3c3c4ddb93
tavi <tavi@osr509>
2023-02-12 10:26:26 +0200

    AOP Around annotation. Documentation could be found here: https://docs.spring.io/spring-framework/docs/2.5.5/reference/aop.html

--- src/main/java/oct/soft/pma/logging/ApplicationLoggerAspect.java
+++ src/main/java/oct/soft/pma/logging/ApplicationLoggerAspect.java
@@ -1,8 +1,9 @@
 package oct.soft.pma.logging;
 
-import org.aspectj.lang.annotation.After;
+import java.util.Arrays;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,13 +14,30 @@ import org.springframework.stereotype.Component;
 public class ApplicationLoggerAspect {
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
-	@Pointcut("within(oct.soft.pma.controllers..*) || within(oct.soft.pma.repositories..*)")
+	@Pointcut("within(oct.soft.pma.controllers..*)")
 	public void definePackagePointcuts() {
 		//empy method just to name the location specified in the pointcut		
 	}
 	
-	@Before("definePackagePointcuts()")
-	public void log() {
-		log.debug(" ------------------------------------------------------ ");
-	}
+	@Around("definePackagePointcuts()")
+	public Object logAround(ProceedingJoinPoint jp) {
+		          log.debug("\n\n\n");
+
+            log.debug("***** Before method execution ******* \n {}.{} () with argument(s) = {}",
+                    jp.getSignature().getDeclaringTypeName(),
+                    jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+            log.debug("___________________________________________________________________\n");
+            Object o=null;
+            try {
+                o = jp.proceed();
+            } catch (Throwable ex) {
+               ex.printStackTrace();
+            }
+            log.debug("***** After method execution ******* \n {}.{} () with argument(s) = {}",
+                    jp.getSignature().getDeclaringTypeName(),
+                    jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+            log.debug("___________________________________________________________________\n");
+            return o;
+        }
+        
 }

e3503df15b2e29e9ee8fd8f88ab2a3ff01250324
octsantau <octavian.santau@plastor.ro>
2023-02-13 13:42:23 +0200

    Add Employee, Project API Controller.

--- src/main/java/oct/soft/pma/api/controllers/ProjectAPIController.java
+++ src/main/java/oct/soft/pma/api/controllers/ProjectAPIController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import oct.soft.pma.entities.Employee;
 import oct.soft.pma.entities.Project;
-import oct.soft.pma.services.EmployeeService;
 import oct.soft.pma.services.ProjectService;
 
 @RestController

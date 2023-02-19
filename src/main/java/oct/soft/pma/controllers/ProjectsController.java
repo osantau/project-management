be9518764d10b983e2c23675036d8302acaad6a8
octsantau <octavian.santau@plastor.ro>
2023-02-19 12:03:41 +0200

    Update and delete projects.
    Modify Create/Update messages in creation forms.

--- src/main/java/oct/soft/pma/controllers/ProjectsController.java
+++ src/main/java/oct/soft/pma/controllers/ProjectsController.java
@@ -1,5 +1,6 @@
 package oct.soft.pma.controllers;
 
+import oct.soft.pma.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import oct.soft.pma.entities.Project;
 import oct.soft.pma.services.EmployeeService;
 import oct.soft.pma.services.ProjectService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/projects")
@@ -40,4 +42,19 @@ public class ProjectsController {
 		// use a redirect to prevent duplicate submissions
 		return "redirect:/projects";
 	}
+          @GetMapping("/update")
+        public String displayProjectUpdateForm(@RequestParam("id") long id, Model model)
+        {
+           Project project = proServ.findById(id);
+            model.addAttribute("project",project);
+            return "projects/new-project";
+        }
+        
+        @GetMapping("/delete")
+        public String deleteProject(@RequestParam("id") long id)
+        {
+            proServ.deleteById(id);
+            return "redirect:/projects";
+        }
+        
 }
